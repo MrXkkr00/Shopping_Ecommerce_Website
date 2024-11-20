69f839c9e5879679a85c433300a4f290e8e4ecbf
MrXkkr00 <rasqadomboyev@gmail.com>
2024-11-21 00:25:57 +0500

    yangi7

--- core/settings/base.py
+++ core/settings/base.py
@@ -36,9 +36,9 @@ LOCAL_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'accounts',
     'products',
+    'orders',
     'common',
 ]
 
