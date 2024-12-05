83156392bb9d3bca52fec4431c7a49b200b421c2
RasulbekQadomboyev <rasqadomboyev@gmail.com>
2024-12-05 12:23:18 +0500

    Update models.py

--- common/models.py
+++ common/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+
+
+class Media(models.Model):
+    class MediaType(models.TextChoices):
+        IMAGE = 'image', _("Image")
+        FILE = 'file', _("File")
+        MUSIC = 'music', _("Music")
+        VIDEO = 'video', _("Video")
+    file = models.FileField(_('File'), upload_to='/files')
+    type = models.CharField(_('Media Type'),max_length=60, choices=MediaType.choices)
+
+
+    def __str__(self):
+        return self.id
+
+
+class Settings
